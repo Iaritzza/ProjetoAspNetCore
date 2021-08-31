6a9a5a75f2aa3194b0b50e505f7fc429b624d612
Iaritzza Bielinki <iaritzzayara@gmail.com>
2021-08-31 08:54:07 -0300

    Projeto criado

---
+++ Projeto001_Curso02/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Projeto001_Curso02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
